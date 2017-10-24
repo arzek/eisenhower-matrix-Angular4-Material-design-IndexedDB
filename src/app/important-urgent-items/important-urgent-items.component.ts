36f8552eb840c32b9081f8d36a57f95058880bc6
Arzek <arzek2015@gmail.com>
2017-10-24 20:40:49 +0300

    Programing 50%

--- src/app/important-urgent-items/important-urgent-items.component.ts
+++ src/app/important-urgent-items/important-urgent-items.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {BaseComponent} from "../base.component";
 
 @Component({
   selector: 'app-important-urgent-items',
   templateUrl: './important-urgent-items.component.html',
   styleUrls: ['./important-urgent-items.component.scss']
 })
-export class ImportantUrgentItemsComponent implements OnInit {
+export class ImportantUrgentItemsComponent extends BaseComponent implements OnInit {
+
+  protected table_name: any = 'important_urgent';
+  protected db_name: any = 'important_urgent';
   constructor() {
+    super();
   }
   ngOnInit() {
+    this.new_item = 'Important and urgent';
+    this.initItem();
+
   }
+
+
 }
