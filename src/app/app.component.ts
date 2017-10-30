a6bea82ec14132b2837d0e09802af12be063acf6
Arzek <arzek2015@gmail.com>
2017-10-30 10:59:00 +0200

    fix: https

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+
+  ngOnInit(): void {
+    if(location.href === 'http://online-eisenhower-matrix.azurewebsites.net' || location.href === 'http://online-eisenhower-matrix.azurewebsites.net/')
+      location.href = 'https://online-eisenhower-matrix.azurewebsites.net';
+  }
 }
